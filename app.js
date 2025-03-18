9aab294b7d685e257e735972ed280579a8d5e3b3
Jeder14 <selene.lun3@gmail.com>
2025-03-18 13:19:42 -0500

    Modificación de h1 y h2

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML=texto;
+    return;
+}
+
+asignarTextoElemento('h1','Challenge del amigo secreto');
+asignarTextoElemento('h2','Digite el nombre de sus reales');

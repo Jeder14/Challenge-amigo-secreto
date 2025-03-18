1cdcbd47921b8e2cc902be29add0bfd31fa6f1d7
Jeder14 <selene.lun3@gmail.com>
2025-03-18 15:49:57 -0500

    Añadir alerta de repetición de nombre y guardado de nombres en la lista

--- app.js
+++ app.js
@@ -1,21 +1,41 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
+let cantidadAmigos = 1 
+let amigos = [];
+
 function asignarTextoElemento(elemento, texto){
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML=texto;
     return;
 }
 function agregarAmigo(){
-    let nuevoAmigo =(document.getElementById ('amigo').value);
-        
+    const inputAmigo =(document.getElementById ('amigo').value);   
+    const nuevoAmigo = inputAmigo.trim();
+
+    console.log(cantidadAmigos)
+
     if (nuevoAmigo === "") {
           asignarTextoElemento ('h2','Agrega un nombre válido')
-         } else {
-            asignarTextoElemento ('h2','¡Será un gran Challenge!')
+         } else if 
+            (amigos.includes(nuevoAmigo)) {
+            alert(`El amigo ${nuevoAmigo} ya está incluido`)
          }
+         else {
+            amigos.push(nuevoAmigo);
+            asignarTextoElemento ('h2',`Agregaste ${cantidadAmigos} ${(cantidadAmigos ===1)? 'amigo':'amigos'}, ¡Será un gran Challenge!`)
+            cantidadAmigos++; 
+        }
+         limpiarCaja();
          console.log(nuevoAmigo);
     return;
 }
+
+function limpiarCaja(){
+    document.getElementById('amigo').value = '';
+
+}
+
+
 
 
 asignarTextoElemento('h1','Challenge del amigo secreto');

be37d1de0d40a7a739ace547a558b6d36301ef2a
Jeder14 <selene.lun3@gmail.com>
2025-03-18 20:09:17 -0500

    Agregar limpieza de caja y aumento y visualizacion de los valores de la lista

--- app.js
+++ app.js
@@ -2,36 +2,33 @@
 
 //Creación de la variable tipo arreglo/lista/vector "amigos"
 let amigos = [];
+let listaNombreSorteado = [];
+let numeroMaximoSorteos = 0;
 
 function asignarTextoElemento(elemento, texto){
     let elementoHTML = document.querySelector(elemento);
-    elementoHTML.innerHTML=texto;
-    return;
+    if (elementoHTML){
+        elementoHTML.innerHTML=texto;
+    }
 }
 function agregarAmigo(){
     const inputAmigo =(document.getElementById ('amigo').value);   
     const nuevoAmigo = inputAmigo.trim();
 
-
     if (nuevoAmigo === "") {
-          asignarTextoElemento ('h2','Agrega un nombre válido')
-         } else if 
-            (amigos.includes(nuevoAmigo)) {
-            alert(`El amigo ${nuevoAmigo} ya está incluido`)
-         }
-         else {
-            //agregar elementos al final de la lista
-            amigos.push(nuevoAmigo);
-            asignarTextoElemento ('h2',`Agregaste ${amigos.length} ${(nuevoAmigo.length ===1)? 'amigo':'amigos'}, ¡Será un gran Challenge!`)
-
-            console.log(nuevoAmigo);
-            actualizarLista();
-            console.log(amigos);
-        }
-        limpiarCaja();
-        
-
-    return;
+        asignarTextoElemento ('h2','Agrega un nombre válido')
+    } else if (amigos.includes(nuevoAmigo)) {
+        alert(`El amigo ${nuevoAmigo} ya está incluido`);
+    } else {
+        //agregar elementos al final de la lista
+         amigos.push(nuevoAmigo);
+         asignarTextoElemento ('h2',`Agregaste ${amigos.length} ${(nuevoAmigo.length ===1)? 'amigo':'amigos'}, ¡Será un gran Challenge!`)
+         console.log(nuevoAmigo);
+         console.log(amigos);
+         actualizarLista();
+         numeroMaximoSorteos = amigos.length;
+    }
+    limpiarCaja();
 }
 
 function limpiarCaja(){
@@ -43,12 +40,15 @@ function limpiarCaja(){
 
 function actualizarLista(){
     const listaAmigos = document.getElementById('listaAmigos');
-    
     listaAmigos.innerHTML = "";
 
+//Agregando nombres a la lista
+    for(let i= 0; i <amigos.length; i++){
+        const li = document.createElement('li');
+        li.textContent = amigos[i];
+        listaAmigos.appendChild(li);
+    }
 }
-
-
 
 asignarTextoElemento('h1','Challenge del amigo secreto');
 asignarTextoElemento('h2','Digite el nombre de sus reales');
